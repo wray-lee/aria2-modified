bf0fcda87b59d58215cfa369190db567c7d1e299
Wray <wray_lee@outlook.com>
2025-06-07 17:20:33 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+
 ./configure --with-ca-bundle='/etc/ssl/certs/ca-certificates.crt' \
   --with-openssl \
   --without-gnutls \
-  --without-libnettle \
-  --without-libgmp \
+  --disable-nls \
   --enable-static=yes \
   --enable-shared=no \
-  LIBS="-lssl -lcrypto -lpthread -ldl \
+  PKG_CONFIG=/bin/false \
+  OPENSSL_CFLAGS="-I/usr/include" \
+  OPENSSL_LIBS="-L/usr/lib/x86_64-linux-gnu -lssl -lcrypto" \
+  LIBS="-lpthread -ldl \
         /usr/lib/x86_64-linux-gnu/libxml2.a \
         /usr/lib/x86_64-linux-gnu/libsqlite3.a \
         /usr/lib/x86_64-linux-gnu/libssh2.a \
